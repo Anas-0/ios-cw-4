91f53918e97e4dd7073b45f640130c7675370fc8
Anas-0 <anas.almutawa1@gmail.com>
2022-03-11 16:32:10 +0300

    Done

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+var greeting = "Hello, playground"
+var name = "anas"
+var number = 1
+var hobby = "design"
+print("hello, my name is \(name) my favorite number is \(number) my favorite hobby is \(hobby)")
